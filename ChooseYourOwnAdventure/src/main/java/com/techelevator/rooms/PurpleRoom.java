ac085a00d1f88af633df967b86163447e03697ac
Anastassia Bowers <anastassia.bowers@gmail.com>
2023-05-26 17:35:38 -0700

    Added some ASCII art to the Green Room, Added a birthdayCake method to Purple Room, Adjusted println text for Purple Intro, Updated options for PurpleRoom to include eatPizza in class variables, options string, and while loop

--- ChooseYourOwnAdventure/src/main/java/com/techelevator/rooms/PurpleRoom.java
+++ ChooseYourOwnAdventure/src/main/java/com/techelevator/rooms/PurpleRoom.java
@@ -5,11 +5,12 @@ import com.techelevator.Player;
 
 public class PurpleRoom {
 
-    private static final String INTRO = "You enter a purple room. You see your friends. There is cake. You have won at life.";
+    private static final String INTRO = "You enter a purple room, and all your friends are there with a big birthday cake. But someone's missing... the guest of honor! Where could they be?";
 
     private static final String OPTION_CAKE = "Eat the cake";
+    private static final String OPTION_PIZZA = "Eat a slice of pizza";
     private static final String OPTION_LEAVE = "Leave room";
-    private static final String[] OPTIONS = {OPTION_CAKE, OPTION_LEAVE};
+    private static final String[] OPTIONS = {OPTION_CAKE, OPTION_PIZZA, OPTION_LEAVE};
 
     public void onEnterRoom(Player player) {
 
@@ -19,6 +20,8 @@ public class PurpleRoom {
 
             String selectedOption = MenuDisplay.prompt(OPTIONS);
             if(selectedOption.equals(OPTION_CAKE)){
+                System.out.println("You can't cut the cake until the guest of honor is here! Perhaps they need rescuing?");
+            }else if(selectedOption.equals(OPTION_PIZZA)){
                 onEatPizza(player);
             }else if(selectedOption.equals(OPTION_LEAVE)){
                 System.out.println("You exit the room");
@@ -36,9 +39,18 @@ public class PurpleRoom {
     //    player.setHealth( player.getHealth() + 20);
     //}
 
-    //Here is the code redone so that it's called "pizza" instead:
+
+    //Here is the code where onEatCake is redone so that it's called "pizza" instead:
     private void onEatPizza(Player player)   {
-        System.out.println("Yum! Delicious pizza! You eat a slice and it restores your health.");
+        System.out.println("Yum, delicious pizza! You eat a slice and it restores your health.");
         player.setHealth( player.getHealth() + 20);
     }
+
+
+    //Below is the revised onEatCake code, redone so it doesn't affect health, just prints a congrats message
+    private void onEatBirthdayCake(Player player) {
+        System.out.println("Sweet victory! You defeated the robot and rescued the guest of honor! Well done, hero.");
+    }
+
+
 }
