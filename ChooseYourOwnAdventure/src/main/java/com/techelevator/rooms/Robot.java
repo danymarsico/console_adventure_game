b0fcd7ecda80edd6bd0cda4f576efb6c56269910
Daniele Marsico <danymarsico@gmail.com>
2023-05-26 21:24:28 -0400

    Creates a YellowRoom and Robot class

---
+++ ChooseYourOwnAdventure/src/main/java/com/techelevator/rooms/Robot.java
@@ -0,0 +1,27 @@
+package com.techelevator.rooms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Robot {
+    private final int MAX_HEALTH = 100;
+    private final int MIN_HEALTH = 0;
+
+    private int health = MAX_HEALTH;
+    private List<String> items = new ArrayList<>();
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        if(health > MAX_HEALTH) {
+            this.health = MAX_HEALTH;
+        } else if (health < MIN_HEALTH) {
+            this.health = MIN_HEALTH;
+        } else {
+            this.health = health;
+        }
+    }
+
+}
