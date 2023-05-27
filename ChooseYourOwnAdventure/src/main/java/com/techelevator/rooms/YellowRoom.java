a02327ca4bf530c8b36d7fb2fb98d53c334fc5c2
Daniele Marsico <danymarsico@gmail.com>
2023-05-27 11:47:15 -0400

    Fixes unlocking yellowRoom access

--- ChooseYourOwnAdventure/src/main/java/com/techelevator/rooms/YellowRoom.java
+++ ChooseYourOwnAdventure/src/main/java/com/techelevator/rooms/YellowRoom.java
@@ -2,22 +2,34 @@ package com.techelevator.rooms;
 
 import com.techelevator.Player;
 import com.techelevator.constants.GameConstants;
+import com.techelevator.menu.MenuDisplay;
 
 public class YellowRoom {
     private static final String INTRO_YELLOW = "The door opens, and you enter the Yellow room";
 
 
     public void onEnterRoom(Player player){
+        while (true) {
+            if(player.hasItem(GameConstants.YELLOW_KEY)){
+                YellowRoom.onOpenDoor(player);
+                break;
+                //System.out.println(INTRO_YELLOW);
+            } else {
+                System.out.println("Hmm, looks like the door requires a key!");
+                break;
+            }
 
+
+        }
     }
 
-    private void onOpenDoor(Player player) {
+    private static void onOpenDoor(Player player) {
         if(player.hasItem(GameConstants.YELLOW_KEY)){
             System.out.println(INTRO_YELLOW);
             System.out.println("Hurraaay! You've saved the guest of honor: the Birthday person! You can go back to celebrate in the Blue room ^_^");
             //yellowRoom.onEnterRoom(player);
-        } else {
+        } /*else {
             System.out.println("Hmm, looks like the door requires a key");
-        }
+        } */
     }
 }
