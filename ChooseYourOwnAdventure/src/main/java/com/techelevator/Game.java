c05996533ae2fb90fc6511b9df841e9942f9fc01
Daniele Marsico <danymarsico@gmail.com>
2023-05-31 22:59:01 -0400

    Creates a window and first elements for a GUI

--- ChooseYourOwnAdventure/src/main/java/com/techelevator/Game.java
+++ ChooseYourOwnAdventure/src/main/java/com/techelevator/Game.java
@@ -2,8 +2,25 @@ package com.techelevator;
 
 import com.techelevator.menu.MenuDisplay;
 import com.techelevator.rooms.*;
+import com.techelevator.rooms.Robot;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
+
+    JFrame window;
+    Container container;
+    JPanel titlePanel, startButtonPanel, mainTextPanel;
+    JLabel titleName;
+    Font titleFont = new Font("Helvetica",Font.PLAIN,55);
+    Font normalFont = new Font("Helvetica", Font.PLAIN, 40);
+    JButton startButton;
+    JTextArea mainTextArea;
+
+    TitleScreenHandler titleScreenHandler = new TitleScreenHandler();
 
     private static final String INTRO = "You are in the lobby of a haunted mansion!";
 
@@ -25,6 +42,63 @@ public class Game {
         Game game = new Game();
         game.beginGame();
 
+    }
+
+    public Game(){
+        window = new JFrame();
+        window.setSize(1200,800);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(null);
+        window.setVisible(true);
+        container = window.getContentPane();
+        titlePanel = new JPanel();
+        titlePanel.setBounds(200,100,800,250);
+        titlePanel.setBackground(Color.black);
+        titleName = new JLabel("BIRTHDAY'S MYSTERY");
+        titleName.setForeground(Color.white);
+        titleName.setFont(titleFont);
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(500, 500,200,100);
+        startButtonPanel.setBackground(Color.black);
+        startButton = new JButton("START");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(normalFont);
+        startButton.addActionListener(titleScreenHandler);
+
+        startButtonPanel.add(startButton);
+        container.add(startButtonPanel);
+
+        titlePanel.add(titleName);
+        container.add(titlePanel);
+
+    }
+
+    public void gameScreen(){
+        titlePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100,100,800,350);
+        mainTextPanel.setBackground(Color.blue);
+        container.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("Game content text will appear here");
+        mainTextArea.setBounds(100,100,600,250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+
+    }
+
+    public class TitleScreenHandler implements ActionListener{
+        public void actionPerformed(ActionEvent event) {
+            gameScreen();
+
+        }
     }
 
     public void beginGame() {
